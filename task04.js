08eed0c58a4e73f00d3779fbf7cb2da951c264d0
xghrtkl <ligar.pandika@gmail.com>
2019-01-21 12:59:02 +0700

    .

---
+++ task04.js
@@ -0,0 +1,6 @@
+var a = function(){ //assign function to var a
+    return 'Hello a!' //return 'Hello a!' as the result
+  }
+  var b = function(){ //assign function to var a
+    return 'Hello b!' //return 'Hello b!' as the result
+  }
---
+++ task05.js
@@ -0,0 +1,6 @@
+var greet = function(){
+    return 'Haydo!'
+};
+
+var salutation = greet();  
+ //calling the greet function and assign it to variable called salutation
