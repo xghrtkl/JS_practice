7ed7a7af762ed2daa118b7669a535ee310ea80d2
xghrtkl <ligar.pandika@gmail.com>
2019-01-28 14:09:11 +0700

    .

---
+++ Freecodecamp/1. Basic Javascript/21-30/28. Appending Variables to Strings.js
@@ -0,0 +1,10 @@
+// Example
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is ";
+ourStr += anAdjective;
+
+// Only change code below this line
+
+var someAdjective = "3 char long";
+var myStr = "Learning to code is " 
+myStr += someAdjective;
