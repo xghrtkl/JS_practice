8db35300901406f95204b3a3a89990eeae9d390a
xghrtkl <ligar.pandika@gmail.com>
2019-01-29 14:49:05 +0700

    .

---
+++ Freecodecamp/1. Basic Javascript/91-100/99. Iterate with JavaScript Do...While Loops.js
@@ -0,0 +1,15 @@
+// Setup
+var myArray = [];
+var i = 10;
+
+// Only change code below this line.
+
+do{
+  myArray.push(i)
+  i++
+} while (i < 5)
+
+// while (i < 5) {
+//   myArray.push(i);
+//   i++;
+// }
